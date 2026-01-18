2518e482866af5bf879ee4785c2c2596a4b6a03c
sofa-29 <dzdz985@mail.ru>
2026-01-18 21:22:22 +0300

    header, aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,94 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Phaser from 'phaser';
 import { FarmGame } from './FarmGame';
 
-function App ()
-{
-    // The sprite can only be moved in the MainMenu Scene
+function App() {
+    const [showGradientLine, setShowGradientLine] = useState(true);
     const [canMoveSprite, setCanMoveSprite] = useState(true);
-    
-    //  References to the FarmGame component (game and scene are exposed)
     const phaserRef = useRef();
     const [spritePosition, setSpritePosition] = useState({ x: 0, y: 0 });
+    
+    const [showSidebar, setShowSidebar] = useState(false);
 
     const changeScene = () => {
-
         const scene = phaserRef.current.scene;
-
-        if (scene)
-        {
+        if (scene) {
             scene.changeScene();
         }
-    }
+    };
 
     const moveSprite = () => {
-
         const scene = phaserRef.current.scene;
-
-        if (scene && scene.scene.key === 'MainMenu')
-        {
-            // Get the update logo position
+        if (scene && scene.scene.key === 'MainMenu') {
             scene.moveLogo(({ x, y }) => {
-
                 setSpritePosition({ x, y });
-
             });
         }
-    }
+    };
 
     const addSprite = () => {
-
         const scene = phaserRef.current.scene;
-
-        if (scene)
-        {
-            // Add more stars
+        if (scene) {
             const x = Phaser.Math.Between(64, scene.scale.width - 64);
             const y = Phaser.Math.Between(64, scene.scale.height - 64);
-
-            //  `add.sprite` is a Phaser GameObjectFactory method and it returns a Sprite Game Object instance
             const star = scene.add.sprite(x, y, 'star');
-
-            //  ... which you can then act upon. Here we create a Phaser Tween to fade the star sprite in and out.
-            //  You could, of course, do this from within the Phaser Scene code, but this is just an example
-            //  showing that Phaser objects and systems can be acted upon from outside of Phaser itself.
-            scene.add.tween({
-                targets: star,
-                duration: 500 + Math.random() * 1000,
-                alpha: 0,
-                yoyo: true,
-                repeat: -1
-            });
+            scene.add.tween({ targets: star, duration: 500 + Math.random() * 1000, alpha: 0, yoyo: true, repeat: -1 });
         }
-    }
+    };
 
-    // Event emitted from the FarmGame component
     const currentScene = (scene) => {
-
-        setCanMoveSprite(scene.scene.key !== 'MainMenu');
+        const key = scene.scene.key;
+        setCanMoveSprite(key !== 'MainMenu');
         
-    }
+        setShowSidebar(key === 'Game');
+        setShowGradientLine(key !== 'Game');
+    };
 
     return (
-        <div id="app">
+        <div id="app" style={{ position: 'relative' }}>
+            {showSidebar && (
+             <aside className="sidebar">
+                <div className="sidebarTitle">HZF</div>
+                <div className="rectangles-container">
+                <div className="rectangle" style={{ '--bottom-color': '#945e77' }}>
+                    <div className="top-left">brains</div>
+                    <div className="top-right">9999+ max count</div>
+                    <div className="bottom-left">
+                        <img src="/assets/pink_brain.png" alt='icon' style={{ width: '38px', height: 'auto', verticalAlign: 'bottom'}}/>
+                        <div class="text">23092</div>
+                    </div>
+                    <img src="/assets/brain.png" alt='icon-2' className="bottom-right-image"/>
+                </div>
+                <div className="rectangle" style={{ '--bottom-color': '#945e77' }}>
+                    <div className="top-left">meat</div>
+                    <div className="top-right">10 meat/sec</div>
+                    <div className="bottom-left">
+                        <img src="/assets/pink_brain.png" alt='icon' style={{ width: '38px', height: 'auto', verticalAlign: 'bottom'}}/>
+                        <div className="text-1">9999999+</div>
+                    </div>
+                </div>
+                <div className="rectangle" style={{ '--bottom-color': '#e8a851' }}>
+                    <div className="top-left">gold</div>
+                    <div className="top-right">BUY</div>
+                    <div className="bottom-left">
+                        <img src="/assets/coin.png" alt='icon' style={{ width: '35px', height: 'auto', verticalAlign: 'bottom'}}/>
+                        <div className="text-1">10002</div>
+                    </div>
+                </div>
+                </div>
+             </aside>
+            )}
 
-            <header>
-                {
-                /*
-                    
-                    Тут будет верхняя менюшка
-                */}
+            <header className='header'>
+                <nav className='nav'>
+                    <button className='nav-button' onClick={(e) => { e.preventDefault(); /* логика для перехода */ }}>home</button>
+                    <button className='nav-button' onClick={(e) => { e.preventDefault(); /* логика */ }}>shop</button>
+                    <button className='nav-button' onClick={(e) => { e.preventDefault(); /* логика */ }}>discord</button>
+                    <button className='nav-button' onClick={(e) => { e.preventDefault(); /* логика */ }}>telegram</button>
+                </nav>
+                {showGradientLine && <div className="gradient-line"></div>}
             </header>
-
-            <aside>
-                {/* 
-                    Тут будет боковая менюшка 
-                */}
-            </aside>
 
             <FarmGame ref={phaserRef} currentActiveScene={currentScene} />
 
